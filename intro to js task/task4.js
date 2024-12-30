b55a23626a5bc1c46b9ce2fdf9c611b841387484
mohammadhossainjoy <joyh2755@gmail.com>
2024-12-30 18:04:51 +0600

    Task4 question add

--- intro to js task/task4.js
+++ intro to js task/task4.js
@@ -1,3 +1,12 @@
+/*Task-4
+What will be the result of the following codes:
+
+var a = isNaN(‘11’);
+
+var a = isNaN(2-10);
+
+Explain your answers. */
+
 var a = isNaN("11");
 console.log(a); //False
 var a = isNaN(2 - 10); // -8
