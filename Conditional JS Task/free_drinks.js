cf598863e2e671b7121cf917da3f1f91c6e07f59
mohammadhossainjoy <joyh2755@gmail.com>
2025-01-17 19:52:34 +0600

    drinks free conditional task add

---
+++ Conditional JS Task/free_drinks.js
@@ -0,0 +1,13 @@
+/***
+
+Free Drinks
+    - Burger more than 500tk: free Coke
+    - Else Coke: 30tk
+*/
+var burgerPrice = 600;
+
+if (burgerPrice > 500) {
+  console.log("free coke");
+} else {
+  console.log("buy coke");
+}
