bbd0c238e411ba952626e69de1372222fa019109
mohammadhossainjoy <joyh2755@gmail.com>
2025-01-17 20:27:47 +0600

    Grade calculator task

---
+++ Conditional JS Task/grade_calculator.js
@@ -0,0 +1,26 @@
+/***
+
+Grade Calculator
+
+Create a simple JavaScript program that takes a student's score as input and returns their corresponding grade based on the following grading scale:
+
+    A: 90-100
+    B: 80-89
+    C: 70-79
+    D: 60-69
+    F: 0-59
+
+***/
+const marks = 88;
+
+if (marks >= 90) {
+  console.log("Grade: A");
+} else if (marks >= 80) {
+  console.log("Grade: B");
+} else if (marks >= 70) {
+  console.log("Grade: C");
+} else if (marks >= 60) {
+  console.log("Grade: D");
+} else if (marks < 60) {
+  console.log("Grade: F");
+}
