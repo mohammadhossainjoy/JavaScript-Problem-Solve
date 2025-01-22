6b6326b8c9601f99ef7ad47f6465f4ed771bd913
mohammadhossainjoy <joyh2755@gmail.com>
2025-01-22 21:52:29 +0600

    even number task  use while loop

--- loop JS Task/While loop/odd_even.js
+++ loop JS Task/While loop/odd_even.js
@@ -5,20 +5,25 @@ Subtask-1:
 Find all the odd numbers from 61 to 100.
 
  */
-
-/***
-
-Subtask-2:
-
-Find all the even numbers from 78 to 98.
-
- */
-
-/*programming hero*/
+console.log("Odd Number are: ");
 let num = 61;
 while (num <= 100) {
   if (num % 2 == !0) {
     console.log(num);
   }
   num++;
+}
+console.log("Even Number are :");
+/*
+Subtask-2:
+
+Find all the even numbers from 78 to 98.
+
+ */
+let numbers = 78;
+while (numbers <= 98) {
+  if (numbers % 2 == 0) {
+    console.log(numbers);
+  }
+  numbers++;
 }
