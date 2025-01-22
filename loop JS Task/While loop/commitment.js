16edb8b597c46b82f87d74e874aca795c0b36328
mohammadhossainjoy <joyh2755@gmail.com>
2025-01-22 21:32:13 +0600

    while loop task

---
+++ loop JS Task/While loop/commitment.js
@@ -0,0 +1,13 @@
+/***
+
+"I will invest at least 6 hrs every single day for next 60 days!" this message 60 times. So display this.
+
+ */
+let count = 1;
+const message =
+  "I will invest at least 6 hrs every single day for next 60 days!";
+
+while (count <= 60) {
+  console.log(message);
+  count++;
+}
