3c320fa4b72d1474385eda97f56b9313aef09193
mohammadhossainjoy <joyh2755@gmail.com>
2025-01-23 13:04:59 +0600

    multiplication table use while loop

---
+++ loop JS Task/While loop/multiplication_table.js
@@ -0,0 +1,14 @@
+/***
+
+As Ersa is learning now, she wants to explore more and more. Tell Ersa to generate a multiplication table for number 5
+
+ */
+
+/*programming hero*/
+let mul = 5;
+let a = 1;
+while (a <= 10) {
+  let multiplication = mul * a;
+  console.log(`${mul} x ${a}= `, multiplication);
+  a++;
+}
