49a6f90b8bb28bd4d76dea25dc528df517fbb353
mohammadhossainjoy <joyh2755@gmail.com>
2025-01-23 13:33:07 +0600

    multiplication table task use for loop

--- loop JS Task/For Loop/multiplication_table.js
+++ loop JS Task/For Loop/multiplication_table.js
@@ -7,7 +7,7 @@ As Ersa is learning now, she wants to explore more and more. Tell Ersa to genera
 /*programming hero*/
 let mul = 5;
 
-for (let a = 1; a <= 10; a++) {
-  let multiplication = mul * a;
-  console.log(`${mul} x ${a}= `, multiplication);
+for (let i = 1; a <= 10; i++) {
+  let multiplication = mul * i;
+  console.log(`${mul} x ${i}= `, multiplication);
 }
