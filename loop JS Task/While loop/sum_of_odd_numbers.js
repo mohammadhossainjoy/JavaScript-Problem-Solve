722dab91282e1e749a234886b356ed603c3415fc
mohammadhossainjoy <joyh2755@gmail.com>
2025-01-23 12:51:54 +0600

    sum odd number use while loop

---
+++ loop JS Task/While loop/sum_of_odd_numbers.js
@@ -0,0 +1,25 @@
+/***
+
+Subtask-1:
+
+Display sum of all the odd numbers from 81 to 131.
+
+ */
+let num = 81;
+let sum = 0;
+while (num <= 131) {
+  if (num % 2 == !0) {
+    sum = sum + num;
+  }
+  num++;
+}
+console.log(sum);
+/*
+
+Subtask-2:
+
+Display sum of all the even numbers from 206 to 311.
+
+ */
+
+/*programming hero*/
