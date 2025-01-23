2605ac59f935dc124b977b16b0c5af418a34ea24
mohammadhossainjoy <joyh2755@gmail.com>
2025-01-23 13:09:17 +0600

    countdown task use while loop

---
+++ loop JS Task/While loop/countdown.js
@@ -0,0 +1,12 @@
+/***
+
+Implement a countdown timer that counts down from 21 to 15.
+
+ */
+
+/*programming hero*/
+let countDown = 21;
+while (countDown >= 15) {
+  console.log(countDown);
+  countDown--;
+}
