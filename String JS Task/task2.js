e062c9e2e8d5fa493a907d1788a61d141c819f42
mohammadhossainjoy <joyh2755@gmail.com>
2025-01-24 12:27:51 +0600

    task 2

---
+++ String JS Task/task2.js
@@ -0,0 +1,7 @@
+/*Task-2:
+Count how many times a string has the letter a or A
+
+ */
+const str = "I AM a Student of A North South University";
+
+console.log("String a has: ", str.match(/a/gi).length);
