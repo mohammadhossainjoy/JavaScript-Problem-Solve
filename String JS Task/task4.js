ec905a33d90ca84a61f76de6e2312f9f0f9e5f5a
mohammadhossainjoy <joyh2755@gmail.com>
2025-01-24 17:36:10 +0600

    task4 string replace

---
+++ String JS Task/task4.js
@@ -0,0 +1,7 @@
+/*Task-4:
+If a given string has either x, replace x by y. if the given string has X, replace it by Y. */
+
+const str = 'Maximum and X-ray'
+
+console.log("Original String:",str)
+console.log(" String:", str.replace("x", "y").replace("X", "Y"));
