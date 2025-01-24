decfbffe50fdc65c8c50ed628018ef119e6cf5ea
mohammadhossainjoy <joyh2755@gmail.com>
2025-01-24 18:16:29 +0600

    not complete task

---
+++ String JS Task/task5.js
@@ -0,0 +1,7 @@
+/*Task-5:
+Capitalize Every first Letter of each word in a String */
+
+// const str = 'i am a student of cse at nsu';
+// let s = str.split(" ");
+// console.log(s.toUpperCase());
+// console.log(str.toUpperCase());
