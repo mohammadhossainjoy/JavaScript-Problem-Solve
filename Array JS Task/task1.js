efb2f324f54231962191fd3113409dbaaf86c12f
mohammadhossainjoy <joyh2755@gmail.com>
2025-01-26 09:05:06 +0600

    Declare an array task solve

---
+++ Array JS Task/task1.js
@@ -0,0 +1,12 @@
+/*1. Declare an array
+Declare an array with 5 elements containing fruits
+console log the 3rd index element
+change the value of the 2nd index element to jambura
+console log the final array
+ */
+
+const fruits = ["apple", "mango", "orange", "lemon", "watermelon"];
+console.log(fruits[2]);
+fruits[1] = "jambura";
+
+console.log("Final array: ", fruits);
